e17d5d6fc83874226cdd8f01d81ef1d7e6a19fed
Amanda-Ruzza <amanda.ruzza@cloudreach.com>
2023-07-11 15:13:14 -0400

    Add logger

--- bucket_obj_upload.py
+++ bucket_obj_upload.py
@@ -1,4 +1,5 @@
 import sys
+import logging
 from google.cloud import storage
 
 def upload_blob(bucket_name, source_file_name, destination_blob_name):
@@ -9,14 +10,16 @@ def upload_blob(bucket_name, source_file_name, destination_blob_name):
 
     blob.upload_from_filename(source_file_name)
 
-    print(f"File {source_file_name} uploaded to {destination_blob_name}.")
+    logging.info(f"File {source_file_name} uploaded to {destination_blob_name}.")
 
 
 
 
 if __name__ == "__main__":
+    logging.getLogger().setLevel(logging.INFO)
     upload_blob(
         bucket_name=sys.argv[1],
         source_file_name=sys.argv[2],
         destination_blob_name=sys.argv[3],
     )
+    
