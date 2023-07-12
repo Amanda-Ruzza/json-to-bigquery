2109d870687c4a07c1092ad28c120e232f31645c
Amanda-Ruzza <amanda.ruzza@cloudreach.com>
2023-07-12 17:40:35 -0400

    Add Schema Auto Detect to bq_load.py

--- bq_load.py
+++ bq_load.py
@@ -6,14 +6,10 @@ client = bigquery.Client()
 table_id = "cr-lab-aruzza-2706230354.seatgeeks.events_nyc"
 
 job_config = bigquery.LoadJobConfig(
-    # figure out the auto-schema
-    # schema=[
-    #     bigquery.SchemaField("name", "STRING"),
-    #     bigquery.SchemaField("post_abbr", "STRING"),
-    # ],
-    source_format=bigquery.SourceFormat.NEWLINE_DELIMITED_JSON,
+    autodetect=True, source_format=bigquery.SourceFormat.NEWLINE_DELIMITED_JSON
 )
-uri = "gs://cloud-samples-data/bigquery/us-states/us-states.json"
+
+uri = "gs://cr-lab-aruzza-ae7140b2-seatgeeks/nyc_band_events2.ndjson"
 
 load_job = client.load_table_from_uri(
     uri,
